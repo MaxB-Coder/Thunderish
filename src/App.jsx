1372312d998692c57569922be29e36b0d8f88c46
MaxB-Coder <maxblaschek72@gmail.com>
2023-11-10 19:33:47 +0000

    Added Static Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import '../public/Thunderish.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="header">
+      <a href="/"><FontAwesomeIcon className='home' icon={faHome} /></a>
+      <img src="../public/Thunderish.png" alt="Thunderish Logo" className='logo' />
+    </div>
     </>
   )
 }
