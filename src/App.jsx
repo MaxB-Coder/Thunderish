ccc5428889a77bab1e6a627e1d58f3b108287eee
MaxB-Coder <maxblaschek72@gmail.com>
2023-11-15 17:14:15 +0000

    Static CSS, background, search, and name

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import { Header } from './components/Header.jsx';
 function App() {
   return (
     <>
-    <Header />
-    <div className="main">
-      <form action="">
-        <input className='search' placeholder='Location...'></input>
-        <input className='searchSubmit' type="submit" value="Search" formAction='url'/>
-      </form>
-      <h2 className='searchName'>New York</h2>
-      <a href=""><img src="https://media.cntraveler.com/photos/63483e15ef943eff59de603a/master/pass/New%20York%20City_GettyImages-1347979016.jpg" alt="New York" className="searchImage" /></a>
+    <div style={{ 
+  backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.2)), url(https://media.cntraveler.com/photos/63483e15ef943eff59de603a/master/pass/New%20York%20City_GettyImages-1347979016.jpg)`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  width: '100vw',
+  height: '100vh',
+  margin:'auto'
+}}>
+      <Header />
     </div>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,13 @@ import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 export const Header = () => {
   return (
-    <div className="header">
-      <a href="/"><FontAwesomeIcon className='home' icon={faHome} /></a>
-      <img src="../public/Thunderish.png" alt="Thunderish Logo" className='logo' />
+    <div className="main">
+    <h2 className='searchName'>New York</h2>
+      <form action="" className='search'>
+        <input className='searchBox' placeholder='Location...'></input>
+        <input className='searchSubmit' type="submit" value="Search" formAction='url'/>
+      </form>
+      {/* <img src="../public/Thunderish.png" alt="Thunderish Logo" className='logo' /> */}
     </div>
   )
 }
