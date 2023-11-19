937b0bbcffa3f429d6698e20a164c1a771c6b7ac
MaxB-Coder <maxblaschek72@gmail.com>
2023-11-19 21:25:23 +0000

    Added data service

---
+++ src/utils/weatherDataService.js
@@ -0,0 +1,11 @@
+// import axios from 'axios';
+
+// export const getProducts = async () => {
+//     try {
+//         const responseData = await axios.get(`http://localhost:3000/products`);
+//         return responseData.data;
+//     }
+//     catch (error) {
+//         return error;
+//     }
+// }
